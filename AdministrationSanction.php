f86ec4231a4ac736e882bd179f4b639b0a14edae
benje <benje@DESKTOP-59EPIH8>
2017-02-10 09:47:26 +0100

    test commit jermhy

--- AdministrationSanction.php
+++ AdministrationSanction.php
@@ -13,7 +13,41 @@
     <tr>
         <td>Bavardage</td>
         <td>Sanction du coup</td>
-        <td>Modif</td>
-        <td>SUPP</td>
+        <td><button class="btn btn-info" data-toggle="modal" data-target="#modif">Modif</button></td>
+        <td><button class="btn btn-danger" data-toggle="modal" data-target="#supp">Supp</button></td>
     </tr>
 </table>
+<div class="modal fade" id="modif" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+<h4 class="modal-title" id="myModalLabel">Modifier</h4>
+</div>
+<div class="modal-body">
+Modifier ici
+</div>
+</div><!-- /.modal-content -->
+</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<div class="modal fade" id="supp" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+<h4 class="modal-title" id="myModalLabel">Supp</h4>
+</div>
+<div class="modal-body">
+Supprimer
+</div>
+</div><!-- /.modal-content -->
+</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<cen
+<button class="btn btn-info" data-toggle="modal" data-target="#myModal">
+  Ajouter une sanction
+</button>
+        
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.js">
+ </script>
+ <script src="assets/js/bootstrap.js"></script>
