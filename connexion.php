103819fd1d75f23fe248df809164c288ab3152cc
Thomas BAILLY <Thomas BAILLY@DESKTOP-QIN0G9C>
2017-02-11 13:53:21 +0100

    Création de la page de connexion

---
+++ connexion.php
@@ -0,0 +1,39 @@
+<head>
+    <title>PPE</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<form margin-top:50px class="form-horizontal">
+  <div class="form-group">
+      <div class="col-sm-5"></div>
+      <div class="col-sm-4" >Bienvenue sur le site internet</div> 
+      <div class="col-sm-3"></div>
+  </div>
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-4 control-label">Email</label>
+    <div class="col-sm-4">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-4 control-label">Password</label>
+    <div class="col-sm-4">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-5 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Remember me
+          
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-5 col-sm-10">
+      <button type="submit" class="btn btn-default">Sign in</button>
+    </div>
+  </div>
+</form>
