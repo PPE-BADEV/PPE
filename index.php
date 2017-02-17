615f9a7fba72e190bccdae949ca433fbcf60e8d9
Thomas BAILLY <Thomas BAILLY@DESKTOP-QIN0G9C>
2017-02-17 09:34:11 +0100

    Création de la page de connexion

--- index.php
+++ index.php
@@ -40,7 +40,6 @@
                         <span class="icon-bar"></span>
                     </button>
 
-                    <a class="navbar-brand" href="index.html">Business Casual</a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
--- pages/administrationClasse.php
+++ pages/administrationClasse.php
@@ -1,5 +1,3 @@
-
-<link href="assets/css/bootstrap.css" rel="stylesheet">
 <center>
 <h2>Administration des classes</h2>
 </center>
@@ -60,7 +58,3 @@ Ajouter une classe
 </div><!-- /.modal-content -->
 </div><!-- /.modal-dialog -->
 </div><!-- /.modal --> 
-
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.js">
- </script>
-<script src="assets/js/bootstrap.js"></script>
