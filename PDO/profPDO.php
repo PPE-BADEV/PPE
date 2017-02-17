27a034c2d5271a4595388652d686e70df13669b6
cleme <cleme@DESKTOP-HFQ9N8Q>
2017-02-17 11:47:59 +0100

    Merge origin/master
    Conflicts:
    	pages/AdminProf.php

--- PDO/profPDO.php
+++ PDO/profPDO.php
@@ -1,1 +1,14 @@
 <?php
+function createAdmin($titre,$description){
+    try
+    {
+        $connection = new PDO("mysql:host=localhost;dbname=".BD.";charset=utf8", USER_BD, PWD_BD);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $requete = $connection->prepare('INSERT INTO sujet(nom,prenom,login,pwd) VALUES(:nom,:prenom,:login,:pwd)');
+        $requete->bindValue(':nom', $nom, PDO::PARAM_STR);
+        $requete->bindValue(':prenom', $prenom, PDO::PARAM_STR);
+        $requete->bindValue(':login', $login PDO::PARAM_STR);
+        $requete->bindValue(':pwd', $pwd PDO::PARAM_STR);
+        $requete->execute();
+        $connection=null;
+        }
---
+++ view/createAdmin.php
@@ -0,0 +1,34 @@
+<div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+        <form action="index.php?action=createStore" method="post">
+            <div class="form-group">
+              <label for="titre">Nom</label>
+              <input type="text" class="form-control" id="titre" name="titre" placeholder="Nom">
+            </div>
+            
+            <div class="form-group">
+              <label for="description">Prénom</label>
+               <input type="text" class="form-control" id="titre" name="titre" placeholder="Prénom">
+            </div>
+            
+            <div class="form-group">
+              <label for="description">Login</label>
+               <input type="text" class="form-control" id="titre" name="titre" placeholder="Login">
+            </div>
+            
+            <div class="form-group">
+              <label for="description">Mot de passe</label>
+               <input type="text" class="form-control" id="titre" name="titre" placeholder="Mot de passe">
+            </div>
+            
+            <div class="form-group">
+              <label for="description">Action</label>
+               <input type="text" class="form-control" id="titre" name="titre" placeholder="Action">
+            </div>
+            
+           <button type="submit" class="btn btn-default">Créer</button>
+        </form>
+        </div>    
+    <div class="col-md-2"></div>    
+</div>    
